e52d2fb5bd138a99cf7b0624240b89a0e7549b81
github username <github email address>
2020-02-02 13:02:54 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -10,4 +11,27 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do 
+    erb :new
+  end
+
+  post '/articles' do 
+    @a = Article.create(params[:article])
+    @articles = Article.all
+    # binding.pry
+    erb :show 
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+    erb :index
+  end
+
+  get '/articles/:id/edit' do
+    erb :index
+  end
+
 end
+
+# rake db:create_migration NAME=create_articles
