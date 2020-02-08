d313c7d903eb4fb514758421a6f6994bb0da5e7f
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-02-07 16:19:02 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles/#{@article.id}"
   end
 
-  post 'articles/:id/delete' do
+  delete '/articles/:id/delete' do
     @article = Article.find(params[:id])
     @article.delete
     redirect to '/articles' 
