d18a5c015d322053ad6358d68fe37d8b71b28f00
mrarthurwhite <mister.arthur.white@gmail.com>
2020-02-09 01:44:00 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,59 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    @articles = Article.all
+    erb :index
   end
+
+# index displays all articles
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  # new displays create article form
+  get '/articles/new' do
+    erb :new
+  end
+
+  #create creates one article
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #show displays one article based on ID in the url
+  get "/articles/:id" do
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  # edit displays edit form based on ID in the url
+  get "/articles/:id/edit" do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  #update modifies an existing article based on ID in the url
+  patch "/articles/:id" do
+    @article = Article.find_by_id(params[:id])
+    #binding.pry
+    @article.update(params["article"])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #update
+  put "/articles/:id" do
+
+  end
+
+
+  # delete
+  delete "/articles/:id" do
+    @article = Article.find_by_id(params[:id])
+    @article.destroy
+    redirect to "/articles"
+  end
+
+
 end
--- config.ru
+++ config.ru
@@ -4,8 +4,8 @@ require './config/environment'
 begin
   fi_check_migration
 
+# so that your app will know how to handle PATCH, PUT, and DELETE requests!
   use Rack::MethodOverride
-
   run ApplicationController
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
