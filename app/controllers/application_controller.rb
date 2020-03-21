66756c90830f733077a2f752612df76d5bfbf186
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-03-21 22:49:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,16 +9,42 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/articles/new' do 
-    erb :new
-   end 
+    erb :new 
   end 
   
-  get '/articles' do 
-    @articles = Article.all 
-    erb :index 
-   end 
+  post '/articles' do 
+    #binding.pry 
+    @article = Article.create(params)
+    #binding.pry
+   redirect "/articles/#{@article.id}"
+    
+  end 
+  
+get '/articles' do 
+  @articles = Article.all 
+  erb :index 
+end 
+
+  
+  get '/articles/:id' do
+   @article = Article.find(params[:id])
+   erb :show 
   end 
 
-  get '/' do
-  end
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end 
+  
+  patch '/articles/:id' do 
+    @article.update(params)
+  end 
+  
+    
+  delete 'articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect '/index'
+  end 
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,5 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-  attr_accessor :title, :content 
-  def initialize(title, content)
-    @title = title
-    @content = content
-  end 
+   
 end
